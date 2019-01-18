939f5c54260f1e7f93ac5a876030e0c781d31808
SycoLogic <39813259+Todd-Horizon@users.noreply.github.com>
2019-01-18 08:26:56 -0500

    Broken

---
+++ broken_m9k_small_arms/lua/autorun/m9k_small_arms_11_07_15.lua
@@ -0,0 +1,353 @@
+/*------------------------------------------------------
+
+If you're reading this, then that mean's you've extracted this addon, probably with intentions 
+of editing it for your own needs, or that you're using a legacy addon.
+
+I have no problem with that, but you must understand that I cannot offer support for legacy addons.
+If you've extracted this addon, I cannot offer any help fixing problems that come up. It's impossible
+to know what you've changed, and thus impossible to know what to fix.
+
+"But Bob!" you might say. "I only changed one thing!" 
+
+Well, that's a shame. Everybody is going to say this, and I know that some of those people will be
+lying to me. The only thing I can do is to refuse support to everyone using legacy addons.
+
+So, by using a legacy addon, you accept the fact that I cannot help fix anything that might be broken.
+
+I know it's tough love, but that's the way it's got to be.
+
+------------------------------------------------------*/
+local icol = Color( 255, 255, 255, 255 ) 
+if CLIENT then
+	killicon.Add( "m9k_coltpython_broken", "vgui/hud/m9k_coltpython_broken", icol  )
+	killicon.Add( "m9k_deagle_broken", "vgui/hud/m9k_deagle_broken", icol  )
+	killicon.Add( "m9k_glock_broken", "vgui/hud/m9k_glock_broken", icol  )
+	killicon.Add( "m9k_vector_broken", "vgui/hud/m9k_vector_broken", icol  )
+	
+
+end
+
+--I'm pretty sure we don't need these anymore...
+--Almost 99 percent sure that's I'm 100 percent sure...
+	
+-- if GetConVar("M9KDisableHolster") == nil then
+	-- CreateConVar("M9KDisableHolster", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Disable my totally worthless and broken holster system? Won't hurt my feelings any. 1 for true, 2 for false. A map change may be required.")
+	-- print("Holster Disable con var created")
+-- end
+
+if GetConVar("DebugM9K") == nil then
+	CreateConVar("DebugM9K", "0", { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Debugging for some m9k stuff, turning it on won't change much.")
+end
+
+if GetConVar("M9KWeaponStrip") == nil then
+	CreateConVar("M9KWeaponStrip", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Allow empty weapon stripping? 1 for true, 0 for false")
+	print("Weapon Strip con var created")
+end
+	
+if GetConVar("M9KDisablePenetration") == nil then
+	CreateConVar("M9KDisablePenetration", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Disable Penetration and Ricochets? 1 for true, 0 for false")
+	print("Penetration/ricochet con var created")
+end
+	
+if GetConVar("M9KDynamicRecoil") == nil then
+	CreateConVar("M9KDynamicRecoil", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Use Aim-modifying recoil? 1 for true, 0 for false")
+	print("Recoil con var created")
+end
+	
+if GetConVar("M9KAmmoDetonation") == nil then
+	CreateConVar("M9KAmmoDetonation", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Enable detonatable M9K Ammo crates? 1 for true, 0 for false.")
+	print("Ammo crate detonation con var created")
+end
+
+if GetConVar("M9KDamageMultiplier") == nil then
+	CreateConVar("M9KDamageMultiplier", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Multiplier for M9K bullet damage.")
+	print("Damage Multiplier con var created")
+end
+
+if GetConVar("M9KDefaultClip") == nil then
+	CreateConVar("M9KDefaultClip", "-1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How many clips will a weapon spawn with? Negative reverts to default values.")
+	print("Default Clip con var created")
+end
+	
+if GetConVar("M9KUniqueSlots") == nil then
+	CreateConVar("M9KUniqueSlots", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Give M9K Weapons unique slots? 1 for true, 2 for false. A map change may be required.")
+	print("Unique Slots con var created")
+end
+	
+if !game.SinglePlayer() then
+
+	if GetConVar("M9KClientGasDisable") == nil then
+		CreateConVar("M9KClientGasDisable", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Turn off gas effect for all clients? 1 for yes, 0 for no. ")
+	end
+	
+	if SERVER then
+	
+		function ClientSideGasDisabler()
+			timer.Create("ClientGasBroadcastTimer", 15, 0, 
+				function() BroadcastLua("RunConsoleCommand(\"M9KGasEffect\", \"0\")") end )
+		end
+	
+		if GetConVar("M9KClientGasDisable"):GetBool() then
+			ClientSideGasDisabler()
+		end
+
+		function M9K_Svr_Gas_Change_Callback(cvar, previous, new)
+			if tobool(new) == true then
+				ClientSideGasDisabler()
+				BroadcastLua("print(\"Gas effects disabled on this server!\")")
+			elseif tobool(new) == false then
+				BroadcastLua("print(\"Gas effects re-enabled on this server.\")")
+				BroadcastLua("print(\"You may turn on M9KGasEffect if you wish.\")")
+				if timer.Exists("ClientGasBroadcastTimer") then
+					timer.Destroy("ClientGasBroadcastTimer")
+				end
+			end				
+		end
+		cvars.AddChangeCallback("M9KClientGasDisable", M9K_Svr_Gas_Change_Callback)
+	
+	end
+	
+	if CLIENT then
+		if GetConVar("M9KGasEffect") == nil then
+			CreateClientConVar("M9KGasEffect", "1", true, true)
+			print("Client-side Gas Effect Con Var created")
+		end		
+	end
+
+else
+	if GetConVar("M9KGasEffect") == nil then
+		CreateConVar("M9KGasEffect", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Use gas effect when shooting? 1 for true, 0 for false")
+		print("Gas effect con var created")
+	end
+end
+
+//Kriss
+sound.Add({
+	name = 			"kriss_vector.Single",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/ump45-1.wav"
+})
+
+sound.Add({
+	name = 			"kriss_vector.Magrelease",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/magrel.mp3"
+})
+
+sound.Add({
+	name = 			"kriss_vector.Clipout",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/clipout.mp3"
+})
+
+sound.Add({
+	name = 			"kriss_vector.Dropclip",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/dropclip.mp3"
+})
+
+sound.Add({
+	name = 			"kriss_vector.Clipin",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/clipin.mp3"
+})
+
+
+sound.Add({
+	name = 			"kriss_vector.Boltpull",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/boltpull.mp3"
+})
+
+sound.Add({
+	name = 			"kriss_vector.unfold",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/Kriss/unfold.mp3"
+})
+
+//colt python
+sound.Add({
+	name = 			"Weapon_ColtPython.Single",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"weapons/coltpython/python-1.wav"
+})
+
+sound.Add({
+	name = 			"WepColtPython.clipdraw",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/coltpython/clipdraw.mp3"
+})
+
+sound.Add({
+	name = 			"WepColtPython.blick",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/coltpython/blick.mp3"
+})
+
+sound.Add({
+	name = 			"WepColtPython.bulletsout",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/coltpython/bulletsout.mp3"
+})
+
+sound.Add({
+	name = 			"WepColtPython.bulletsin",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/coltpython/bulletsin.mp3"
+})
+
+//glock 18
+sound.Add({
+	name = 			"Dmgfok_glock.Single",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"weapons/dmg_glock/mac10-1.wav" 
+})
+
+sound.Add({
+	name = 			"Dmgfok_glock.Clipout",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/dmg_glock/magout.mp3" 
+})
+
+sound.Add({
+	name = 			"Dmgfok_glock.clipin",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/dmg_glock/magin.mp3" 
+})
+
+sound.Add({
+	name = 			"Dmgfok_glock.Boltpull",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/dmg_glock/boltpull.mp3" 
+})
+
+sound.Add({
+	name = 			"Dmgfok_glock.Boltrelease",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/dmg_glock/boltrelease.mp3" 
+})
+
+sound.Add({
+	name = 			"Dmgfok_glock.Deploy",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/dmg_glock/mac10_deploy.mp3" 
+})
+
+//desert eagle
+sound.Add({
+	name = 			"Weapon_TDegle.Single",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"weapons/fokku_tc_deagle/deagle-1.wav" 
+})
+
+sound.Add({
+	name = 			"Weapon_TDegle.Clipout",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/fokku_tc_deagle/de_clipout.mp3" 
+})
+
+sound.Add({
+	name = 			"Weapon_TDegle.Clipin",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/fokku_tc_deagle/de_clipin.mp3" 
+})
+
+sound.Add({
+	name = 			"Weapon_TDegle.Slideback",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/fokku_tc_deagle/de_slideback.mp3" 
+})
+
+sound.Add({
+	name = 			"Weapon_TDegle.Deploy",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			"weapons/fokku_tc_deagle/de_deploy.mp3" 
+})
+
+m9knpw = {}
+table.insert(m9knpw, "m9k_davy_crockett_explo")
+table.insert(m9knpw, "m9k_gdcwa_matador_90mm")
+table.insert(m9knpw, "m9k_gdcwa_rpg_heat")
+table.insert(m9knpw, "m9k_improvised_explosive")
+table.insert(m9knpw, "m9k_launched_davycrockett")
+table.insert(m9knpw, "m9k_launched_ex41")
+table.insert(m9knpw, "m9k_launched_m79")
+table.insert(m9knpw, "m9k_m202_rocket")
+table.insert(m9knpw, "m9k_mad_c4")
+table.insert(m9knpw, "m9k_milkor_nade")
+table.insert(m9knpw, "m9k_nervegasnade")
+table.insert(m9knpw, "m9k_nitro_vapor")
+table.insert(m9knpw, "m9k_oribital_cannon")
+table.insert(m9knpw, "m9k_poison_parent")
+table.insert(m9knpw, "m9k_proxy")
+table.insert(m9knpw, "m9k_released_poison")
+table.insert(m9knpw, "m9k_sent_nuke_radiation")
+table.insert(m9knpw, "m9k_thrown_harpoon")
+table.insert(m9knpw, "m9k_thrown_knife")
+table.insert(m9knpw, "m9k_thrown_m61")
+table.insert(m9knpw, "m9k_thrown_nitrox")
+table.insert(m9knpw, "m9k_thrown_spec_knife")
+table.insert(m9knpw, "m9k_thrown_sticky_grenade")
+table.insert(m9knpw, "bb_dod_bazooka_rocket")
+table.insert(m9knpw, "bb_dod_panzershreck_rocket")
+table.insert(m9knpw, "bb_garand_riflenade")
+table.insert(m9knpw, "bb_k98_riflenade")
+table.insert(m9knpw, "bb_planted_dod_tnt")
+table.insert(m9knpw, "bb_thrownalliedfrag")
+table.insert(m9knpw, "bb_thrownaxisfrag")
+table.insert(m9knpw, "bb_thrownsmoke_axis")
+table.insert(m9knpw, "bb_thrownaxisfrag")
+table.insert(m9knpw, "bb_planted_alt_c4")
+table.insert(m9knpw, "bb_planted_css_c4")
+table.insert(m9knpw, "bb_throwncssfrag")
+table.insert(m9knpw, "bb_throwncsssmoke")
+table.insert(m9knpw, "m9k_ammo_357")
+table.insert(m9knpw, "m9k_ammo_pistol")
+table.insert(m9knpw, "m9k_ammo_smg")
+
+function PocketM9KWeapons(ply, wep)
+
+	if not IsValid(wep) then return end
+	class = wep:GetClass()
+	m9knopocket = false
+	
+	for k, v in pairs(m9knpw) do
+		if v == class then
+			m9knopocket = true
+			break
+		end
+	end
+	
+	if m9knopocket then
+		return false
+	end
+	
+	--goddammit i hate darkrp
+	
+end
+hook.Add("canPocket", "PocketM9KWeapons", PocketM9KWeapons )
+
+small_autorun_mounted = true
